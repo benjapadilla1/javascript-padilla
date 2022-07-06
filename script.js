947a5515395777d707f227bc01d9fe0fc66812e2
benjapadilla1 <104010099+benjapadilla1@users.noreply.github.com>
2022-07-05 22:23:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-do{
-    let = respuesta = parseInt(prompt("ingresa un numero menor a 10"))
-}while(respuesta>10 || isNaN(respuesta)){
-    alert("ingreso un numero menor a 10") 
+// Calcular pagos en cuotas sobre un monto determinado.
+
+do {
+    valortotal = parseInt (prompt("Ingrese su monto total"))
+    if(isNaN(valortotal)){
+        alert("Ingrese numeros válidos")
+    }
+    if(valortotal <= 100){
+        alert("Su monto no puede ser menor o igual a 100")
+    }
+}while ((isNaN(valortotal)) || (valortotal<=100)  
+)
+function cuotas(){
+    let meses_cuotas= parseInt(prompt("Ingrese el numero de cuotas deseadas(Las cuotas no pueden ser menor a 1) "))
+    if (isNaN(meses_cuotas)){
+        alert("Ingresa un valor correcto!!")
+        alert("Por favor reinicie la pagina")
+    }
+    if(meses_cuotas<=0){
+        alert("Las cuotas no pueden ser menor o igual a 0")
+        alert("Por favor reinicie la pagina")
+    }
+    if (meses_cuotas >0){
+        alert("La cantidad a abonar con cuotas es de "+valortotal/meses_cuotas)
+    }  
 }
+cuotas()
