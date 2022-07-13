84534fccd8bac42c188c63625775ac1ff7173f0b
benjapadilla1 <104010099+benjapadilla1@users.noreply.github.com>
2022-07-12 23:05:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-// Calcular pagos en cuotas sobre un monto determinado.
+class Auto {
+    constructor(marca, modelo, precio) {
+        this.marca = marca
+        this.modelo = modelo
+        this.precio = precio
+    }
+
+}
+const Auto1 = new Auto("BMW", "X6", 179900);
+const Auto2 = new Auto("Audi", "R8", 186500);
+const Auto3 = new Auto("Ford", "Territory", 41600);
+const Auto4 = new Auto("Toyota", "Yaris GR", 60000);
 
 do {
-    valortotal = parseInt (prompt("Ingrese su monto total"))
-    if(isNaN(valortotal)){
-        alert("Ingrese numeros válidos")
+    autobuscado = prompt("Ingrese el auto que desee, nuestras marcas son los siguientes: BMW , Audi, Ford, Toyota").toLowerCase()
+    if (autobuscado !== "bmw" && autobuscado !== "audi" && autobuscado !== "ford" && autobuscado !== "toyota") {
+        alert("Por favor ingrese una marca de las previamente mencionadas")
+    } if (autobuscado=="bmw"){
+        alert(`El unico modelo que nos queda de la marca ${Auto1.marca} es el ${Auto1.modelo}, y cuesta ${Auto1.precio} dólares`)
     }
-    if(valortotal <= 100){
-        alert("Su monto no puede ser menor o igual a 100")
+    else if (autobuscado=="audi"){
+        alert(`El unico modelo que nos queda de la marca ${Auto2.marca} es el ${Auto2.modelo}, y cuesta ${Auto2.precio} dólares`)
     }
-}while ((isNaN(valortotal)) || (valortotal<=100)  
-)
-function cuotas(){
-    let meses_cuotas= parseInt(prompt("Ingrese el numero de cuotas deseadas(Las cuotas no pueden ser menor a 1) "))
-    if (isNaN(meses_cuotas)){
-        alert("Ingresa un valor correcto!!")
-        alert("Por favor reinicie la pagina")
+    else if (autobuscado=="ford"){
+        alert(`El unico modelo que nos queda de la marca ${Auto3.marca} es el ${Auto3.modelo}, y cuesta ${Auto3.precio} dólares`)
     }
-    if(meses_cuotas<=0){
-        alert("Las cuotas no pueden ser menor o igual a 0")
-        alert("Por favor reinicie la pagina")
+    else if (autobuscado=="toyota"){
+        alert(`El unico modelo que nos queda de la marca ${Auto4.marca} es el ${Auto4.modelo}, y cuesta ${Auto4.precio} dólares`)
     }
-    if (meses_cuotas >0){
-        alert("La cantidad a abonar con cuotas es de "+valortotal/meses_cuotas)
-    }  
-}
-cuotas()
+} while (autobuscado !== "bmw" && autobuscado !== "audi" && autobuscado !== "ford" && autobuscado !== "toyota")
+
+const Autos = [Auto1, Auto2, Auto3, Auto4]
+
