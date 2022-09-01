9923be22b4889f21a721704180e3f895f1faed95
benjapadilla1 <104010099+benjapadilla1@users.noreply.github.com>
2022-08-31 23:45:53 -0300

    Proyecto final

--- script.js
+++ script.js
@@ -40,27 +40,27 @@ fetch('productos.json').then(function (response) {
 })
 
 class Producto {
-  constructor(nombre, precio, precio_coutas, categoria, imagen, boton) {
+  constructor(nombre, informacion, precio, precio_coutas, categoria, imagen, boton) {
     this.nombre = nombre
+    this.informacion = informacion
     this.precio = precio
     this.precio_coutas = precio_coutas
     this.categoria = categoria
     this.imagen = imagen
     this.boton = boton
   }
-
 }
-const placa1 = new Producto("PLACA DE VIDEO PALIT NVIDIA GEFORCE RTX 3080TI GAMING PRO 12GB GDDR6X 384 BIT LHR", 254099, "12 cuotas sin interés de $27950.92", "placas", "fotos/placa_1.jpg", `<button id= boton_placa1 class="btn btn-primary">Comprar</button> `)
-const placa2 = new Producto("PLACA DE VIDEO PALIT NVIDIA GEFORCE RTX 3080 TI GAMEROCK 12GB GDDR6 384BIT LHR", 323399, "12 cuotas sin interés de $35573.92", "placas", "fotos/placa_2.jpg", `<button id= boton_placa2 class="btn btn-primary">Comprar</button> `)
-const placa3 = new Producto("PLACA DE VIDEO MSI NVIDIA GEFORCE RTX 3080TI GAMING X TRIO 12GB LHR", 329169, "12 cuotas sin interés de $36208.67", "placas", "fotos/placa_3.jpg", `<button id= boton_placa3 class="btn btn-primary">Comprar</button> `)
-const placa4 = new Producto("PLACA DE VIDEO MSI NVIDIA GEFORCE RTX 3080 TI VENTUS 3X OC 12GB GDDR6 LHR", 334949, "12 cuotas sin interés de $36844.42", "placas", "fotos/placa_4.jpg", `<button id= boton_placa4 class="btn btn-primary">Comprar</button> `)
+const placa1 = new Producto("PLACA DE VIDEO PALIT NVIDIA", "GEFORCE RTX 3080TI GAMING PRO 12GB GDDR6X 384 BIT LHR", 254.099, "12 cuotas sin interés de $27950.92", "placas", "fotos/placa_1.jpg", `<button id= boton_placa1 class="btn btn-primary">Comprar</button> `)
+const placa2 = new Producto("PLACA DE VIDEO PALIT NVIDIA", "GEFORCE RTX 3080 TI GAMEROCK 12GB GDDR6 384BIT LHR", 323.399, "12 cuotas sin interés de $35573.92", "placas", "fotos/placa_2.jpg", `<button id= boton_placa2 class="btn btn-primary">Comprar</button> `)
+const placa3 = new Producto("PLACA DE VIDEO MSI NVIDIA", "GEFORCE RTX 3080TI GAMING X TRIO 12GB LHR", 329.169, "12 cuotas sin interés de $36208.67", "placas", "fotos/placa_3.jpg", `<button id= boton_placa3 class="btn btn-primary">Comprar</button> `)
+const placa4 = new Producto("PLACA DE VIDEO MSI NVIDIA", "GEFORCE RTX 3080 TI VENTUS 3X OC 12GB GDDR6 LHR", 334.949, "12 cuotas sin interés de $36844.42", "placas", "fotos/placa_4.jpg", `<button id= boton_placa4 class="btn btn-primary">Comprar</button> `)
 
 const placas = [placa1, placa2, placa3, placa4]
 
-const monitor1 = new Producto("MONITOR 32 VIEWSONIC ELITE XG320U 150HZ 4K", 346.499, "12 cuotas sin interés de $38114.92", "monitor", "fotos/monitor_1.png",)
-const monitor2 = new Producto("MONITOR 49 SAMSUNG LC49J890D SUPER ULTRAWIDE CURVO GAMING 144HZ", 259.859, "259859,12 cuotas sin interés de $28584", "monitor", "fotos/monitor_2.png",)
-const monitor3 = new Producto("MONITOR 27 ZOWIE XL2746K LED", 202.109, "12 cuotas sin interés de $22232", "monitor", "fotos/monitor_3.png",)
-const monitor4 = new Producto("MONITOR 27 VIEWSONIC ELITE XG270QG 165HZ", 187.099, "12 cuotas sin interés de $20580.92", "monitor", "fotos/monitor_4.png",)
+const monitor1 = new Producto("MONITOR 32 VIEWSONIC ", "ELITE XG320U 150HZ 4K", 346.499, "12 cuotas sin interés de $38114.92", "monitor", "fotos/monitor_1.png",)
+const monitor2 = new Producto("MONITOR 49 SAMSUNG ", "LC49J890D SUPER ULTRAWIDE CURVO GAMING 144HZ", 259.859, "259859,12 cuotas sin interés de $28584", "monitor", "fotos/monitor_2.png",)
+const monitor3 = new Producto("MONITOR 27 ZOWIE ", "XL2746K LED", 202.109, "12 cuotas sin interés de $22232", "monitor", "fotos/monitor_3.png",)
+const monitor4 = new Producto("MONITOR 27 VIEWSONIC ", "ELITE XG270QG 165HZ", 187.099, "12 cuotas sin interés de $20580.92", "monitor", "fotos/monitor_4.png",)
 
 const monitores = [monitor1, monitor2, monitor3, monitor4]
 
@@ -81,6 +81,7 @@ for (let placa of placas) {
   <div class="col-12 col-md-6">
       <div class="item shadow mb-4">
           <h3 class="item-title">${placa.nombre}</h3>
+          <h3>${placa.informacion}</h3>
           <h3>${placa.precio_coutas}</h3>
           <img class="item-image" src=${placa.imagen}>
           <div class="item-details">
@@ -98,6 +99,7 @@ for (let monitor of monitores) {
   <div class="col-12 col-md-6">
       <div class="item shadow mb-4">
           <h3 class="item-title">${monitor.nombre}</h3>
+          <h3>${monitor.informacion}</h3>
           <h3>${monitor.precio_coutas}</h3>
           <img class="item-image" src=${monitor.imagen}>
           <div class="item-details">
@@ -112,23 +114,6 @@ let carro_de_compra = document.getElementById("carro_de_compra")
 carro_de_compra.innerHTML += `<section class="shopping-cart">
 <div class="container">
     <h1 class="text-center">CARRITO</h1>
-    <div class="row">
-        <div class="col-6">
-            <div class="shopping-cart-header">
-                <h6>Producto</h6>
-            </div>
-        </div>
-        <div class="col-2"> 
-            <div class="shopping-cart-header">
-                <h6 class="text-truncate">Precio</h6>
-            </div>
-        </div>
-        <div class="col-4">
-            <div class="shopping-cart-header">
-                <h6>Cantidad</h6>
-            </div>
-        </div>
-    </div>
     <div class="shopping-cart-items shoppingCartItemsContainer">
     </div>
     <div class="row">
@@ -165,6 +150,8 @@ botonesAñadirAlCarrito.forEach((añadirBoton) => {
 
 const comprarButton = document.querySelector('.comprarButton');
 comprarButton.addEventListener('click', comprarButtonClicked);
+
+
 
 const shoppingCartItemsContainer = document.querySelector(
   '.shoppingCartItemsContainer'
@@ -204,7 +191,9 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
         <div class="col-6">
             <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
                 <img src=${itemImage} class="shopping-cart-image">
-                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+                <h6 id="titulo-carrito" class="shopping-cart-item-title" >${itemTitle}</h6>
+                <hr>
+                <hr>
             </div>
         </div>
         <div class="col-2">
@@ -217,7 +206,7 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
                 class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
                 <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
                     value="1">
-                <button class="btn btn-danger buttonDelete" type="button">X</button>
+                <button id="borrar" class="btn btn-danger buttonDelete"  type="button">X</button>
             </div>
         </div>
     </div>`;
@@ -256,15 +245,19 @@ function updateShoppingCartTotal() {
     );
     total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
   });
-  shoppingCartTotal.innerHTML = `${total.toFixed(2)}$`;
+  shoppingCartTotal.innerHTML = `$${total}`;
 }
 
 function removeShoppingCartItem(event) {
   const buttonClicked = event.target;
   buttonClicked.closest('.shoppingCartItem').remove();
   updateShoppingCartTotal();
-}
+  Toastify({
+    text: "Se ha eliminado el producto del carrito correctamente",
+    duration: 3000,
+  }).showToast();
 
+}
 function quantityChanged(event) {
   const input = event.target;
   input.value <= 0 ? (input.value = 1) : null;
